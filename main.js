07583430e68aa315080b8455b9502de5e1f7f09d
Pedro Lino de Lima <77000970+PedroLinoLima@users.noreply.github.com>
2021-05-14 10:09:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -31,18 +31,6 @@ const links = {
         {
             "titulo": "calculadora",
             "url": "https://pedrolinolima.github.io/Calculadora_JS/"
-        },
-        {
-            "titulo": "lâmpada",
-            "url": "https://pedrolinolima.github.io/Lampada.js/"
-        },
-        {
-            "titulo": "semáforo",
-            "url": "https://pedrolinolima.github.io/Semaforo.js/"
-        },
-        {
-            "titulo": "todo-list",
-            "url": "https://pedrolinolima.github.io/todo-list/"
         }
     ]
 }
